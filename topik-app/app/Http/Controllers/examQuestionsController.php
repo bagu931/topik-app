98719e5e6029e49015bc8e1e366b176d5d795b6c
Chu3 <mrto9x@gmail.com>
2022-10-19 17:04:22 +0900

    test commit

--- topik-app/app/Http/Controllers/examQuestionsController.php
+++ topik-app/app/Http/Controllers/examQuestionsController.php
@@ -14,7 +14,7 @@ class examQuestionsController extends Controller
     public function index()
     {
         //
-        return view('examQuestions');
+       
     }
 
     /**
--- topik-app/resources/views/examQuestions.blade.php
+++ topik-app/resources/views/examQuestions.blade.php
@@ -7,26 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    // $ch = curl_init();
-    // $url = '/daegubank/ib/dgb/v1/inquiry/balamt_inq';
-    // curl_setopt($ch, CURLOPT_URL, $url);
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    // curl_setopt($ch, CURLOPT_HEADER, FALSE);
-    // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, '{
-    //   "dataHeader": {
-    //     "PRCS_RSLT_DVCD": "0"
-    //   },
-    //   "dataBody": {
-    //     "RCV_ACNO": "508141868135"
-    //   }
-    // }');
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, array('appkey:testkey', 'authorization:1a302af1-b8c0477-48f195a-820a3c-5292', 'glob_id:20171017oaswbd11183412345000020200', ));
-    // $response = curl_exec($ch);
-    // curl_close($ch);
-    
-    // var_dump($response);
-    ?>
+   
 </body>
 </html>
